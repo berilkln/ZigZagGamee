d36057029744b8cb4eca83e6d3db6504018b7275
Beril Kartal <108799436+berilkln@users.noreply.github.com>
2023-05-05 10:12:04 +0300

    Player Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    Vector3 yon = Vector3.left;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(yon.x == 0) //z ekseninde hareket
+            {
+                yon = Vector3.left;
+            }
+            else
+            {
+                yon = Vector3.back; 
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 hareket = yon * speed * Time.deltaTime; //objenin hareket değeri.
+        transform.position += hareket; //hareket değeri sürekli pozisyona eklenir.
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
