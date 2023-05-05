ed2453beddce9cec13912c5b7e19c1e94f0d8d86
Beril Kartal <108799436+berilkln@users.noreply.github.com>
2023-05-05 14:26:09 +0300

    Best Score, PlayerPrefs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,21 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed;
-    [SerializeField] Text scoreText;
+    [SerializeField] Text scoreText,bestScoreTest;
     Vector3 yon = Vector3.left;
     public GroundSpawner groundSpawner;
     public static bool isDead = false;
     public float hizlanmaZorlugu;
     float score = 0f;
     float artisMiktari = 1f;
-    
+    int bestScore = 0;
+
+    private void Start()
+    {
+        bestScore = PlayerPrefs.GetInt("BestScore");   //yapılan en iyi skoru hafızada tutar.
+        bestScoreTest.text = "Best: " + bestScore.ToString();
+    }
+
 
 
 
@@ -39,13 +46,25 @@ public class PlayerController : MonoBehaviour
         if(transform.position.y < 0.1f)
         {
             isDead = true;
-            
+            if (bestScore < score)
+            {
+                bestScore = (int)score;
+                PlayerPrefs.SetInt("BestScore", bestScore);
+
+            }
             Destroy(this.gameObject,3f);
+
+
         }
     }
 
     private void FixedUpdate()
     {
+
+        if (isDead) //karakter öldüyse aşağıdaki işlemler çalışmaz.
+        {
+            return;
+        }
         Vector3 hareket = yon * speed * Time.deltaTime; //objenin hareket değeri.
         speed += Time.deltaTime * hizlanmaZorlugu;
         transform.position += hareket; //hareket değeri sürekli pozisyona eklenir.
